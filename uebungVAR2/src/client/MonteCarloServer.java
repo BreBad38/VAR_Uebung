47a2374f6e2c059bcd63a46067918283694ebd31
Henrik Lawall <henrik.lawall@web.de>
2016-05-11 19:24:40 +0200

    Deletion

--- uebungVAR2/src/client/MonteCarloServer.java
+++
@@ -1,9 +0,0 @@
-package client;
-
-import java.math.BigDecimal;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface MonteCarloServer extends Remote {
-    public BigDecimal berechneZufallstropfen() throws RemoteException;
-}
