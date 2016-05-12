ffbd031289325b2197bc2c41cb9eeda72910e1bf
Pathen85 <henrik.lawall@web.de>
2016-05-12 11:54:37 +0200

    Added comments

--- client/src/client/MonteCarloClient.java
+++ client/src/client/MonteCarloClient.java
@@ -7,26 +7,34 @@ import server.MonteCarloServer;
 
 public class MonteCarloClient {
     public static void main(final String[] args) {
+        // Anlegen und Konfigurieren des Security Managers, falls noch nicht geschehen
         if (System.getSecurityManager() == null) {
             System.setProperty("java.security.policy", "policy\\rmi.policy");
             System.setSecurityManager(new SecurityManager());
         }
+
         MonteCarloServer server;
         BigDecimal[] piValues = new BigDecimal[args.length];
         try {
+            // Jeder Server wird mit der Berechnung einer Pi-Annäherung beauftragt
             for (int i = 0; i < args.length; i++) {
                 server = (MonteCarloServer) Naming.lookup("//" + args[i] + "/ComputePi");
+                // Die Pi-Werte werden in ein Array geschrieben
                 piValues[i] = server.berechnePi();
             }
         } catch (Exception e) {
             System.out.println("Hoppla, da ist etwas schiefgelaufen...");
             e.printStackTrace();
         }
+
         BigDecimal pi = new BigDecimal(0);
+        // Die Pi-Annäherungen werden aufaddiert...
         for (BigDecimal bigDecimal : piValues) {
             pi = pi.add(bigDecimal);
         }
+        // ...und dann durch ihre Anzahl geteilt, um einen Mittelwert zu ermitteln
         pi = pi.divide(new BigDecimal(piValues.length));
+        System.out.println("Die Annäherung für Pi lautet:");
         System.out.println(pi);
     }
 }
--- server/src/server/StartServer.java
+++ server/src/server/StartServer.java
@@ -7,21 +7,23 @@ import java.rmi.registry.Registry;
 public class StartServer {
 
     public static void main(final String[] args) {
+        // Der Security Manager wird konfiguriert und gestartet, falls noch nicht geschehen
         if (System.getSecurityManager() == null) {
             System.setProperty("java.security.policy", "policy\\rmi.policy");
             System.setSecurityManager(new SecurityManager());
         }
 
+        // Erstellen der Registry
         try {
             LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
         } catch (Exception e) {
             System.out.println("Could not create RMIRegistry");
             e.printStackTrace();
         }
-        // Server Startup
+
+        // Jeder Server erhält einen Namen und eine Angabe für die Genauigkeit
         String name = "//" + args[0] + "/ComputePi";
         int genauigkeit = Integer.parseInt(args[1]);
-
         try {
             Naming.rebind(name, new MonteCarloServerImpl(genauigkeit));
             System.out.println("Server (re)bound");
